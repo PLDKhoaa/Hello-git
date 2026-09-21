84ebd5a0158bb160c84b02ddfd9c85a360b146af
PLDKhoaa <dangk4512@gmail.com>
2026-09-21 11:07:11 +0700

    linkedlist

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct Node{
+    int val;
+    Node* next;
+};
+
+struct LinkedList {
+    Node* head;
+    Node* tail;
+};
 int main() {
     int a, b;
     cin >>a >>b;
